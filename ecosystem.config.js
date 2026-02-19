01b4cd4032a21e2bd3fe3ed3d5a4eacd322309a9
Mohammad Akbary <mohammad_aky@yahoo.com>
2026-02-20 03:18:44 +0330

    fix issue that PM2 is using the system Python

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,12 @@ module.exports = {
     {
       name: "mtproto-proxy-bot",
       script: "main.py",
-      interpreter: "python3",
+      interpreter: "/root/Mtproto-proxy-bot/venv/bin/python3",
       watch: false,
       autorestart: true,
       max_restarts: 10,
       restart_delay: 5000,
-      env: {
-        NODE_ENV: "production",
-      },
+      cwd: "/root/Mtproto-proxy-bot",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
       out_file: "logs/pm2-out.log",
       error_file: "logs/pm2-error.log",
